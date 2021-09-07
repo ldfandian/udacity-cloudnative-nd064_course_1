752c0298bb8f955a5d30134d22fe8d895950af9a
Dian FAN <ldfandian@hotmail.com>
2021-09-08 01:59:49 +0800

    adding logging support

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import json
+import logging
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    ## log line
+    app.logger.info('Main request successfull')
     return "Hello World!"
 
 @app.route("/status")
@@ -18,6 +21,9 @@ def getStatus():
         status=200,
         mimetype='application/json'
     )
+
+    ## log line
+    app.logger.info('Status request successfull')
     return response
 
 @app.route("/metrics")
@@ -36,7 +42,12 @@ def getMetrics():
         status=200,
         mimetype='application/json'
     )
+
+    ## log line
+    app.logger.info('Metrics request successfull')
     return response
 
 if __name__ == "__main__":
+    ## stream logs to app.log file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
