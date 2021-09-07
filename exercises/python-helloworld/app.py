78bf4aea0a294dfd17b632f64c9e991783c37d23
Dian FAN <ldfandian@hotmail.com>
2021-09-08 01:42:31 +0800

    support /status and /metrics

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+import json
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def getStatus():
+    return json.dumps({
+        'result':'OK - healthy'
+    })
+
+@app.route("/metrics")
+def getMetrics():
+    return json.dumps({
+        'data': {
+            'userCount': 140,
+            'userCountActive': 23
+        }
+    })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
